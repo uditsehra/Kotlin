d82ae70ee1e8c1d0bc32275c3fe13054ac9eccb0
Udit Sehra <uditsehra@gmail.com>
2020-09-11 23:41:03 +0530

    Higher Order Function example

--- Intro -  Kotlin For Beginners/06-HigherOrderFunctions.kt
+++ Intro -  Kotlin For Beginners/06-HigherOrderFunctions.kt
@@ -2,7 +2,24 @@
 A higher order function is a function that takes function as parameters, or returns a function.
 Lamda functions are also higher order functions.
 */
-
 fun main(){
+    var name:String = "Budda"
+    higherOrderFunctionExample(name,{
 
+    })
 }
+
+
+fun doNothing(isActive:Boolean):Unit{
+    println("Hello")
+}
+
+fun higherOrderFunctionExample(name:String, doNothing:(isActive:Boolean)->Unit):String{
+    if(name == "Buuda"){
+        doNothing(true)
+    }else{
+        doNothing(false)
+    }
+    return "Hi! $name\nHow are you"
+}
+
